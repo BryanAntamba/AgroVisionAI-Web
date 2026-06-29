3f262ef81ce8ce2bee7d00b4a24aa7264e40eca2
ALAN-SVV <alansvela2019@gmail.com>
2026-06-29 07:56:47 -0500

    Cambios de maiccol, color barras de navegacion de roles, y la gestion de rutas de autenticacion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,16 +2,15 @@ import { Routes } from '@angular/router';
 import { Login } from './autenticacion/login/login';
 import { PanelAdmin } from './admin/panel-admin/panel-admin';
 import { Recomendaciones } from './admin/recomendaciones/recomendaciones';
-import { PlataformaEditable } from './admin/plataforma-editable/plataforma-editable';
 import { PanelAgricultor } from './agricultor/panel-agricultor/panel-agricultor';
 import { Historial } from './agricultor/historial/historial';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'panel-admin', component: PanelAdmin },
-  { path: 'panel-admin/recomendaciones', component: Recomendaciones },
-  { path: 'panel-admin/editar-plataforma', component: PlataformaEditable },
-  { path: 'panel-agricultor', component: PanelAgricultor },
-  { path: 'historial', component: Historial },
+  { path: 'panel-admin', component: PanelAdmin, canActivate: [AuthGuard] },
+  { path: 'panel-admin/recomendaciones', component: Recomendaciones, canActivate: [AuthGuard] },
+  { path: 'panel-agricultor', component: PanelAgricultor, canActivate: [AuthGuard] },
+  { path: 'historial', component: Historial, canActivate: [AuthGuard] },
 ];
